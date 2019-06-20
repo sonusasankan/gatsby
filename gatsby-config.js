1878cd1d6ed4af0034070e3a24650f65dbff4dd3
sonusasankan <sonusparsham@gmail.com>
2019-06-20 17:03:23 +0530

    Change name of env variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ try {
 // Overwrite the Contentful config with environment variables if they exist
 contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || contentfulConfig.accessToken,
   host: process.env.CONTENTFUL_HOST
 }
 
